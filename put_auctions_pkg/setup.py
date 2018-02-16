359d0bce155c1a9bc91839c79f0bb296b44d7abc
godsdog <an.andrusyk@gmail.com>
2018-02-16 14:25:15 +0200

    Add configparser to requirements

--- put_auctions_pkg/setup.py
+++ put_auctions_pkg/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
 setup(name='put_auctions',
       py_modules=['put_auctions'],
-      install_requires=['python-dateutil', 'gevent', 'iso8601'])
+      install_requires=['python-dateutil', 'gevent', 'iso8601',
+                        'configparser'])
